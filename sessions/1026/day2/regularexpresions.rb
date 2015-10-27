943c95307ef42ed76cce3a1e1f49e49d74e123d3
Justin Reese <justin.x.reese@gmail.com>
2015-10-27 10:11:36 -0500

    Adds examples of string split and date regex

--- sessions/1026/day2/regularexpresions.rb
+++ sessions/1026/day2/regularexpresions.rb
@@ -2,6 +2,8 @@ source_file = File.open 'client_data.csv'
 regex = /(?<name_first>\w.+\b)\s(?<name_last>\w.+\b)\s,/
 
 source_file.each_line do |source_line|
+  split_items = source_line.split(',')
+  date_regex = /,(?<date>\d+-\d+-\d+)$/
   match = regex.match source_line
   puts match[:name_first] rescue nil
 end
